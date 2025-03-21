5c34c9f99f86cbf70e4878b4d606d923e9edd981
Jose38782 <A01666969@tec.mx>
2025-03-21 00:25:51 -0600

    Codigo de snake con colores aletorios tanto para la serpiente y la comida

--- snake.py
+++ snake.py
@@ -7,7 +7,8 @@ Exercises
 3. How would you move the food?
 4. Change the snake to respond to mouse clicks.
 """
-
+#Bibliotecas
+import random
 from random import randrange
 from turtle import *
 
@@ -17,6 +18,28 @@ food = vector(0, 0)
 snake = [vector(10, 0)]
 aim = vector(0, -10)
 
+#coloca colores random del 0 al 4
+colorSnake=random.randint(0,4)
+colorFood=random.randint(0,4)
+
+#condiones en caso de el color sea el mismo
+if(colorSnake==colorFood):
+    if(colorSnake==4):
+        colorSnake-=1
+    else:
+        colorSnake+=1
+
+#lista de los colores
+colors = {
+    0: "black",
+    1: "purple",
+    2: "green",
+    3: "blue",
+    4: "brown",
+}
+
+colorSnake = colors[colorSnake]
+colorFood = colors[colorFood]
 
 def change(x, y):
     """Change snake direction."""
@@ -51,9 +74,9 @@ def move():
     clear()
 
     for body in snake:
-        square(body.x, body.y, 9, 'black')
+        square(body.x, body.y, 9, colorSnake)
 
-    square(food.x, food.y, 9, 'green')
+    square(food.x, food.y, 9, colorFood)
     update()
     ontimer(move, 100)
 
